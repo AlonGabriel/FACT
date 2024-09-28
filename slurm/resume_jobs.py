47f797189bb02a1c1b360bcd5c43aa09d3654dc6
Mohammad Farahmand <m.farahmand@queensu.ca>
2024-09-28 00:39:25 -0400

    Added support for exclusion by tag.

--- slurm/resume_jobs.py
+++ slurm/resume_jobs.py
@@ -7,11 +7,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Resumes experiments from a given project.')
     parser.add_argument('project', type=str, help='Project name')
     parser.add_argument('--state', type=str, nargs='+', required=True)
+    parser.add_argument('--exclude', type=str, nargs='+')
     parser.add_argument('--job-name', type=str, required=True)
     parser.add_argument('--time-limit', type=str, default='2:00:00')
     parser.add_argument('--directives', type=str, nargs='+')
     parser.add_argument('--working-dir', type=str, default=os.getcwd())
     args = parser.parse_args()
+
+    excluded_tags = args.exclude or []
 
     directives = '\n'.join(f'#SBATCH --{el}' for el in (args.directives or []))
 
@@ -22,6 +25,8 @@ if __name__ == '__main__':
     while iterator.more:
         run = iterator.next()
         if run.state in args.state:
+            if any(tag in run.tags for tag in excluded_tags):
+                continue
             job = template.format(
                 job_name=args.job_name,
                 time=args.time_limit,
